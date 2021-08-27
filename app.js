14c80ac5edd997cb41015ba0788ce87b76cde488
dingdata <dingbro@gmail.com>
2021-08-27 14:20:14 +0800

    Updated App.js with Trainer

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const express = require("express");
 const db = require("./db/models/index");
 const app = express();
-const pokemonRouter = require("./routes/pokemons.route");
 
 app.use(express.json());
+
+// app.js Using a cookie Parser
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+const pokemonRouter = require("./routes/pokemons.route");
 app.use("/pokemon", pokemonRouter);
+
+const trainersRouter = require("./routes/trainers.js");
+app.use("/trainers", trainersRoutes);
 
 db.sequelize.sync();
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
+
 // default error handler
 app.use((err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   res.status(err.statusCode).send(err.message);
 });
-
-// app.js Using a cookie Parser
-const cookieParser = require("cookie-parser");
-app.use(cookieParser());
 
 module.exports = app;
