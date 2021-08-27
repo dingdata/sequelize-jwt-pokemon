f04c98556403becfb9a5aad55e0673b6f3446658
dingdata <dingbro@gmail.com>
2021-08-27 15:16:49 +0800

    Added protect Route and JWT.js

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-require("dotenv").config();
+require("dotenv").config(); // JWT requires this.
 
 const express = require("express");
 const db = require("./db/models/index");
+const cookieParser = require("cookie-parser");
+const pokemonRouter = require("./routes/pokemons.route");
+const trainersRouter = require("./routes/trainers.js");
 const app = express();
 
 app.use(express.json());
 
 // app.js Using a cookie Parser
-const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
-const pokemonRouter = require("./routes/pokemons.route");
 app.use("/pokemon", pokemonRouter);
-
-const trainersRouter = require("./routes/trainers.js");
 app.use("/trainers", trainersRouter);
 
 db.sequelize.sync();
--- config/jwt.js
+++ config/jwt.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const getJWTSecret = () => {
+  // retrieving secret key
   const secret = process.env.JWT_SECRET_KEY;
   if (!secret) {
     throw new Error("Missing secrets to sign JWT token");
@@ -13,9 +14,13 @@ const createJWTToken = (username) => {
   const exp = new Date(today);
 
   const secret = getJWTSecret();
-  exp.setDate(today.getDate() + 60); // adding days
+  exp.setDate(today.getDate() + 60); // set expiry date on payload + 2 months
 
+  // set expiry date on payload
   const payload = { username: username, exp: parseInt(exp.getTime() / 1000) };
+
+  //jwt.sign(payload, secret, {expiresIn: "7d"}) // alternative methods
+
   const token = jwt.sign(payload, secret);
   return token;
 };
