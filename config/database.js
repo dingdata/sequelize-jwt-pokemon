bd80bdb44ed49a6de4f3b46d974d84fb30eaeb1a
dingdata <dingbro@gmail.com>
2021-08-27 10:03:20 +0800

    Bootstrap express app - Database Done

---
+++ config/database.js
@@ -0,0 +1,26 @@
+// config/database.js
+
+module.exports = {
+  development: {
+    username: "postgres",
+    password: null,
+    database: "sequelize_jwt_pokemon_dev",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  test: {
+    username: "postgres",
+    password: null,
+    database: "sequelize_jwt_pokemon_dev_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: "postgres",
+  },
+};
