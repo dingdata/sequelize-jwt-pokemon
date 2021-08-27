c83dc2578a30cde24b1d539d1198bcac6e8c2724
dingdata <dingbro@gmail.com>
2021-08-27 09:55:34 +0800

    Bootstrap express app

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+// default error handler
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  res.status(err.statusCode).send(err.message);
+});
+
+module.exports = app;
